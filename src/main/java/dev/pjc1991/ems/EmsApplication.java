0eea99ca0d9e6a7062d97d7896cb33f7d87d2b4c
JC Park <whdekf@naver.com>
2023-08-28 23:19:07 +0900

    Initial commit

---
+++ src/main/java/dev/pjc1991/ems/EmsApplication.java
@@ -0,0 +1,13 @@
+package dev.pjc1991.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pjc1991/ems/EmsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pjc1991.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

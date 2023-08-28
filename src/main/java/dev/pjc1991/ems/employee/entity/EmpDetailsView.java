1fe9a5c55af7fe952c9c83ce934134997b553aa7
pjc1991 <whdekf@gmail.com>
2023-08-29 01:14:32 +0900

    CHORE : 불필요한 import 삭제

--- src/main/java/dev/pjc1991/ems/employee/entity/EmpDetailsView.java
+++ src/main/java/dev/pjc1991/ems/employee/entity/EmpDetailsView.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Immutable;
 
 import java.math.BigDecimal;
 
